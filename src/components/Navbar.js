5c90f291a2ac8a79f59d513cc6803e406e7ed6ed
saikiran2111 <saikiran211101@gmail.com>
2024-10-28 17:49:38 +0530

    Completed navbar responsiveness for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,67 +1,86 @@
-import React from 'react';
-import { Link } from 'react-scroll';  // Import Link from react-scroll
-import '../styles/Navbar.css';  
+import React, { useState } from 'react';
+import { Link } from 'react-scroll';
+import '../styles/Navbar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse, faAddressCard, faHandHoldingHeart } from '@fortawesome/free-solid-svg-icons';
+import { faHouse, faAddressCard, faHandHoldingHeart, faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <div className="nav-bar">
       <h1 className="hotel-name">NCR Conventions</h1>
-      <ul className="nav-links">
-        <li>
-          <FontAwesomeIcon icon={faHouse} style={{ color: '#000000' }} />
-          <Link 
-            activeClass="active" 
-            to="home"   // Link to the home section
-            spy={true} 
-            smooth={true} 
-            offset={-70}  // Adjust if you have a fixed navbar
-            duration={100}
-          >
-            Home
-          </Link>
-        </li>
-        <li>
-          <FontAwesomeIcon icon={faAddressCard} style={{ color: '#000000' }} />
-          <Link 
-            activeClass="active" 
-            to="about-us"   // Ensure you have an "about" section
-            spy={true} 
-            smooth={true} 
-            offset={-70} 
-            duration={100}
-          >
-            About
-          </Link>
-        </li>
-        <li>
-          <FontAwesomeIcon icon={faHandHoldingHeart} style={{ color: '#000000' }} />
-          <Link 
-            activeClass="active" 
-            to="services"   // Ensure you have a "services" section
-            spy={true} 
-            smooth={true} 
-            offset={-70} 
-            duration={100}
-          >
-            Services
-          </Link>
-        </li>
-        <li>
-          <FontAwesomeIcon icon={faAddressCard} style={{ color: '#000000' }} />
-          <Link 
-            activeClass="active" 
-            to="contact-us"  // Ensure you have a "contact" section
-            spy={true} 
-            smooth={true} 
-            offset={-70} 
-            duration={100}
-          >
-            Contact
-          </Link>
-        </li>
-      </ul>
+      <div className="menu-container">
+        <ul className={`nav-links ${isMenuOpen ? 'active' : ''}`}>
+          <li>
+            <FontAwesomeIcon icon={faHouse} style={{ color: '#000000' }} />
+            <Link 
+              activeClass="active" 
+              to="home"
+              spy={true} 
+              smooth={true} 
+              offset={-70}  
+              duration={100}
+              onClick={closeMenu}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faAddressCard} style={{ color: '#000000' }} />
+            <Link 
+              activeClass="active" 
+              to="about-us"  
+              spy={true} 
+              smooth={true} 
+              offset={-70} 
+              duration={100}
+              onClick={closeMenu}
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faHandHoldingHeart} style={{ color: '#000000' }} />
+            <Link 
+              activeClass="active" 
+              to="services"  
+              spy={true} 
+              smooth={true} 
+              offset={-70} 
+              duration={100}
+              onClick={closeMenu}
+            >
+              Services
+            </Link>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faAddressCard} style={{ color: '#000000' }} />
+            <Link 
+              activeClass="active" 
+              to="contact-us"  
+              spy={true} 
+              smooth={true} 
+              offset={-70} 
+              duration={100}
+              onClick={closeMenu}
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+        <button className="menu-icon" onClick={toggleMenu}>
+          <FontAwesomeIcon icon={isMenuOpen ? faTimes : faBars} />
+        </button>
+      </div>
     </div>
   );
 };
